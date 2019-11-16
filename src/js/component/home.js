ac0dad85773ff6980d1f80f6186b1973e58c4019
sgonzalo <santiago.gonzalo360@gmail.com>
2019-11-16 16:55:38 +0000

    first push

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,45 @@
 import React from "react";
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+import 'bootstrap';
+import '../styles/index.scss';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+ReactDOM.render(
+    <SimpleCounter />,
+    document.querySelector('#app')
+);
 
-//create your first component
-export function Home() {
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+function SimpleCounter (props){
+    return (<div className="bigCounter">
+                <div className="calendar">
+                    <i className="far fa-clock"></i>
+                </div>
+                    <div className="four">{props.digitFour % 10}</div>
+                    <div className="three">{props.digitThree % 10}</div>
+                    <div className="two">{props.digitTwo % 10}</div>
+                    <div className="one">{props.digitOne % 10}</div>
+            </div>);
 }
+SimpleCounter.propTypes = {
+  digitFour: PropTypes.number,
+  digitThree: PropTypes.number,
+  digitTwo: PropTypes.number,
+  digitOne: PropTypes.number
+};
+  
+let counter = 0;
+
+setInterval(function(){
+    const four = Math.floor(counter/1000);
+    const three = Math.floor(counter/100);
+    const two= Math.floor(counter/10);
+    const one = Math.floor(counter/1);
+    
+    console.log(four, three, two, one);
+    
+    counter++;
+    ReactDOM.render(
+        <SimpleCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four}  />,
+        document.querySelector('#app')
+    );
+},1000);
